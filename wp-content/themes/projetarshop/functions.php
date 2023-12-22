a3830963a0414dfe23313d88107e2e43dc38edb3
Inaldo Monteiro <inaldomonteiroti@gmail.com>
2023-12-22 17:56:47 -0300

    Ajustando Aula 40

--- wp-content/themes/projetarshop/functions.php
+++ wp-content/themes/projetarshop/functions.php
@@ -52,6 +52,22 @@ if( class_exists( 'WooCommerce' )){
 	require get_template_directory() . '/inc/wc-modifications.php';
 }
 
+/**
+ * Show cart contents / total Ajax
+ */
+add_filter( 'woocommerce_add_to_cart_fragments', 'projetarshop_woocommerce_header_add_to_cart_fragment' );
+
+function projetarshop_woocommerce_header_add_to_cart_fragment( $fragments ) {
+	global $woocommerce;
+
+	ob_start();
+
+	?>
+	<span class="items"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+	<?php
+	$fragments['span.items'] = ob_get_clean();
+	return $fragments;
+}
 
 
 
